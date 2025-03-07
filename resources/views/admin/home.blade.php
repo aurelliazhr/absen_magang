7ae42d7bdb2a6d3b0877104267447c673249d300
h0om4n71030@gmail.com <h0om4n71030@email.com>
2025-03-07 14:00:32 +0800

    admin update 2

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,21 @@
+<style>
+  .chart-container {
+    width: 50%;
+    height: 50%;
+    margin: auto;
+  }
+</style>
 @extends('templateAdmin')
 
 @section('admin')
-<h1>hello!!</h1>
+
+<!-- <div class="container mx-auto mb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6"> -->
+    <div class="bg-white shadow rounded p-6 text-center border border-blue-500">
+        <h2 class="text-xl font-bold text-black-800">Guru</h2>
+    </div>
+    <div class="bg-white shadow rounded p-6 text-center border border-blue-500" style="background-color: green;">
+        <h2 class="text-xl font-bold text-black-800">Siswa</h2>
+    </div>
+<!-- </div> -->
+
 @endsection
--- resources/views/admin/lihat_guru.blade.php
+++ resources/views/admin/lihat_guru.blade.php
@@ -6,25 +6,24 @@
     color: #FFFFFF;
     font-weight: bold;
     font-size: 20px;
+    margin-top: 2%;
   }
-  /* #nama{
-    
-  } */
+  #Gform{
+    margin-top: 4%;
+  }
 </style>
 @extends('templateAdmin')
 
 @section('admin')
-<h1 class="fw-bold text-center mt-3" id="judul">Data siswa</h1>
-<div class="container m-5 d-flex justify-content-center">
+<h1 class="fw-bold text-center mt-5" id="judul">Data pembimbing</h1>
+<div class="container d-flex justify-content-center" id="Gform">
   <img src="https://placehold.co/300x250" class="rounded float-start" id="logo" alt="sementara" style="width: 300px;">
   <div class="col-md-6 ms-5 mt-5">
-      <input type="text" class="form-control form-control-lg h-59" placeholder="Nama siswa" style="border-color: black;" id="nama">
+      <input type="text" class="form-control form-control-lg h-59" placeholder="Nama pembimbing" style="border-color: black;" id="nama">
       <br>
-      <input type="email" class="form-control form-control-lg h-59" placeholder="siswa@gmail.com" style="border-color: black;" id="email">
+      <input type="email" class="form-control form-control-lg h-59" placeholder="pembimbing@gmail.com" style="border-color: black;" id="email">
       <br>
       <input type="password" class="form-control form-control-lg h-59" placeholder="masukkan kata sandi" style="border-color: black;" id="pass">
-      <br>
-      <input type="text" class="form-control form-control-lg h-59" placeholder="SMKN 2 Banjarmasin" style="border-color: black;" id="asal">
       <br>
       <div class="position-relative text-center" style="width: 350px;">
        <button type="button" class="btn btn-light position-absolute pull-right" id="btnM">Kembali</button>
