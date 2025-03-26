33f26556f166b16012851c293db4276a56cb7e96
h0om4n71030@gmail.com <h0om4n71030@email.com>
2025-03-26 14:06:37 +0800

    hapus btn rekap&jurnal

--- resources/views/pembimbing/home.blade.php
+++ resources/views/pembimbing/home.blade.php
@@ -1,30 +1,8 @@
-<style>
-    #btn1{
-        height: 80%;
-        width: 100%;
-        font-weight: bold;
-        font-size: 28px;
-    }
-    #btn2{
-        height: 80%;
-        width: 100%;
-        font-weight: bold;
-        font-size: 28px;
-    }
-</style>
 @extends('templateGuru')
 
-@section('guru')
-
-<div class="container-fluid px-4 d-flex justify-content-center" id="container1" style="text-align:center;">
-    <div class="col-md d-flex gap-5">
-        <a href="#" class="btn btn-success mt-4 text-nowrap" id="btn1" ><i class="bi bi-download"></i> Rekap absen</a>
-        <a href="#" class="btn btn-success mt-4 text-nowrap" id="btn2" ><i class="bi bi-download"></i> Rekap tugas</a>
-    </div>
-</div>
-    
-    <h5 class="fw-bold text-start mt-5 ms-5">Siswa bimbingan</h1>
-    <div class="table-responsive container mt-5 d-flex justify-content-center">
+@section('guru')    
+    <h5 class="fw-bold text-start mt-3 ms-5">Siswa bimbingan</h1>
+    <div class="table-responsive container mt-4 d-flex justify-content-center">
         <table class="table text-center">
             <tbody>
              @forelse ($users as $u)
